f21e11c83e685e138b260c71ad8b4fa0dee54400
naveenbatakurki-tamu <naveenbatakurki@tamu.edu>
2026-02-01 20:05:01 -0600

    i have add the code

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,9 @@
-Repository Structure (Required)
-Create a Git repository with the following directory structure:
-project1-git-demo/
-├── README.md
-├── src/
-│ ├── main.cpp
-│ └── solver.cpp
-├── include/
-│ └── solver.h
-├── data/
-│ └── input.dat
-├── scripts/
-│ └── run.sh
-└── notes/
-└── ideas.txt
-The files do not need to contain meaningful code initially. Placeholder content is acceptable.
+// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
+
